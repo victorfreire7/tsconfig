2de20199bef21bdd79be109c1b2fffcbfaa77de0
victorfreire7 <vhugo.freire159@gmail.com>
2026-02-05 14:11:24 -0300

    first commit

---
+++ src/001 types.ts
@@ -0,0 +1,21 @@
+// Variables
+
+let nome: string = 'Victor';
+let idade: number = 10;
+let adulto: boolean = true;
+
+// Arrays
+let arrayNumber: Array<number> = [1, 2, 3];
+let ArrayNumber2: number[] = [1, 2, 3];
+
+// Objects
+let person: {nome: string, idade: number, adulto?: boolean} = {
+  idade: 30,
+  nome: 'Victor'
+};
+
+//Function
+function soma(x: number, y: number): number {
+  return x + y;
+}
+
