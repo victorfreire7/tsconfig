60d48a048c8a20d7aa8e9300df3f2b0031a5bcb8
victorfreire7 <vhugo.freire159@gmail.com>
2026-02-09 19:10:01 -0300

    first commit

--- dist/002 any.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// any é um tipo que engloba todos os outros.
-// "falta de tipo.
-// caso um valor nao tenha o type explicito, ele se torna any.
-function show(msg) {
-    return console.log(msg);
-}
-show('olá');
-show([1, 2, 3]);
--- src/007 never.ts
+++
@@ -1,9 +0,0 @@
-// Never:
-// quando uma funçao nao retorna / Mesma funçao do Void
-// geralmente usada pra criação de erros.
-
-export function newErro(): never {
-  throw new Error('Random Error');
-}
-
-newErro();
